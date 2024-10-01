dafbc74159bb46807687613c8e53c1813e16b45a
Tushar Utane <tusharutane2@gmail.com>
2024-10-01 10:03:45 +0530

    Added Aboutus page and footer section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-7xl mx-auto bg-gray-900 text-gray-50 `}>
 				<Header />
 				{children}
+				<Footer />
 			</body>
 		</html>
 	);
